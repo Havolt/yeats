5a1d2d0db4465e63548b1adc21ee692b441d3b0d
Mark <markfitz815@gmail.com>
2024-02-23 22:29:35 +0000

    Update header component and normalize rem

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import './App.css'
 
-import Header from './components/Header/Header'
+import SiteHeader from './components/SiteHeader/SiteHeader'
 
 function App() {
 
   return (
     <div id="app">
-      <Header />
+      <SiteHeader />
     </div>
   )
 }
---
+++ src/components/SiteHeader/SiteHeader.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./SiteHeader.css"
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header className="site-header">
+      <h1>Yeats Art Class</h1>
+      <span>One step every day</span>
+    </header>
+  )
+}
+
+export default Header
