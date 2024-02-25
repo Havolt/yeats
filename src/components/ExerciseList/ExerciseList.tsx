253fafc9536c93cfe6c50b65a99c1ae788df3971
Mark <markfitz815@gmail.com>
2024-02-25 22:32:27 +0000

    Render some basic html for the exercise list

--- src/components/ExerciseList/ExerciseList.tsx
+++ src/components/ExerciseList/ExerciseList.tsx
@@ -6,14 +6,26 @@ type ExerciseGroups = {
     lineTypes: boolean;
     drawingPrompts: boolean;
     gestureDescriptions: boolean;
+    [key: string]: boolean;
 }
 
 type Props = {
     exerciseGroups: ExerciseGroups;
 }
 
+const updateExerciseGroup = () => {
+    // Toggle the state of the ExerciseGroups
+}
+
 export default function ExerciseList({exerciseGroups}: Props) {
   return (
-    <div>ExerciseList</div>
+    <ul>
+        {Object.keys(exerciseGroups).map((key) => (
+        <li key={key}>
+            <label htmlFor={key}>{key}</label>
+            <input type="checkbox" name={key} checked={exerciseGroups[key]} onChange={updateExerciseGroup} />
+        </li>
+      ))}
+    </ul>
   )
 }
